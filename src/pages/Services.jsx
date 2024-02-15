2386fb9bc3d0e1d247ca39f4be0bb2987550e310
armorbita <armorbita@gmail.com>
2024-02-15 18:03:39 +0400

    service description comunications

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -8,6 +8,35 @@ import serviveInner4 from '../assets/img/service10.png';
 import { useNavigate } from 'react-router-dom';
 import { motion } from "framer-motion";
 
+
+const ServiceData = [
+    {
+        id: 'service1',
+        name : 'Skilled nursing care',
+        description : `Our hospice's dedication to offering complete assistance for individuals with life-limiting illnesses is based on our provision of expert nursing care. Our committed nursing staff puts our patients' physical, emotional, and spiritual needs first, focusing on their whole well-being. ${<br />}${<br />} Our qualified nurses are excellent at handling difficult medical conditions, giving prescriptions, and working with other healthcare professionals to create individualised care plans. They keep a close eye on any changes in their patients' health and are experts in pain, symptom control, and end-of-life care.`,
+        image : serviveInner1,
+    },
+    {
+        id: 'service2',
+        name : 'Skilled nursing care',
+        description : "Our hospice's dedication to offering complete assistance for individuals with life-limiting illnesses is based on our provision of expert nursing care. Our committed nursing staff puts our patients' physical, emotional, and spiritual needs first, focusing on their whole well-being. <br /><br /> Our qualified nurses are excellent at handling difficult medical conditions, giving prescriptions, and working with other healthcare professionals to create individualised care plans. They keep a close eye on any changes in their patients' health and are experts in pain, symptom control, and end-of-life care.",
+        image : serviveInner2,
+    },
+    {
+        id: 'service3',
+        name : 'Skilled nursing care',
+        description : "Our hospice's dedication to offering complete assistance for individuals with life-limiting illnesses is based on our provision of expert nursing care. Our committed nursing staff puts our patients' physical, emotional, and spiritual needs first, focusing on their whole well-being. <br /><br /> Our qualified nurses are excellent at handling difficult medical conditions, giving prescriptions, and working with other healthcare professionals to create individualised care plans. They keep a close eye on any changes in their patients' health and are experts in pain, symptom control, and end-of-life care.",
+        image : serviveInner3,
+    },
+    {
+        id: 'service4',
+        name : 'Skilled nursing care',
+        description : "Our hosing with othe' health and are experts in pain, symptom control, and end-of-life care.",
+        image : serviveInner4,
+    },
+
+]
+
 const Services = () => {
 
     let navigate = useNavigate();
@@ -15,6 +44,12 @@ const Services = () => {
     const handleClickSchedul=(e)=>{
         e.preventDefault();
         navigate('/contactUs', { replace: true });
+    }
+    const handleDescDropdown=(e , serviceId)=>{
+        e.preventDefault();
+        console.log(serviceId ,  document.getElementById(serviceId));
+        document.getElementById(serviceId).classList.add('active')
+
     }
 
     useEffect(() => {
@@ -39,116 +74,29 @@ const Services = () => {
                 <div className="section_title center_mode">Our Services</div>
             </div>
             <div className="services_list page_section ">
-                <div className="goals_section service_block inlineImg_section second_bg" id="service1" >
-                    <div className="inlineImg_container">
-                        <div className="image_block absoluite_image" style={{ backgroundImage: `url(${serviveInner1})` }}></div>
+            {ServiceData.map((service)=>(
+               <div key={service.id} className="goals_section service_block inlineImg_section second_bg"  >
+                   <div className="inlineImg_container">
+                        <div className="image_block absoluite_image" style={{ backgroundImage: `url(${service.image})` }}></div>
                         <div className="goal_info page_section">
                             <div className="info_inner">
                                 <div className="section_title service_title">
                                     Skilled nursing care
                                 </div>
-                                <div className="section_description">
-                                    "Our hospice's dedication to offering complete assistance for individuals with
-                                    life-limiting illnesses is based on our provision of expert nursing care. Our
-                                    committed nursing staff puts our patients' physical, emotional, and spiritual
-                                    needs first, focusing on their whole well-being.
-                                    <br />
-                                    <br />
-                                    Our qualified nurses are excellent at handling difficult medical conditions,
-                                    giving prescriptions, and working with other healthcare professionals to
-                                    create individualised care plans. They keep a close eye on any changes
-                                    in their patients' health and are experts in pain, symptom control, and
-                                    end-of-life care.
+                                <div className="section_description" style={{ height: "280px", overflow: "hidden" }} id={service.id} >
+                                    {service.description}
                                 </div>
                                 <div className="buttons_line">
-                                    <a href="/#" className="seeMore">See more{">"} </a>
+                                    {service.description.length > 300 &&
+                                        <a href="/#" onClick={(e)=>handleDescDropdown(e , service.id)} className="seeMore">See more{">"} </a>
+                                    }
                                     <a href="/#" onClick={(e)=>handleClickSchedul(e)} className="site_btn">Schedul</a>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="goals_section  service_block inlineImg_section second_bg" id="service2" >
-                    <div className="inlineImg_container">
-                        <div className="image_block absoluite_image" style={{ backgroundImage: `url(${serviveInner2})` }}></div>
-                        <div className="goal_info page_section">
-                            <div className="info_inner">
-                                <div className="section_title service_title">
-                                    Scheduled visits
-                                </div>
-                                <div className="section_description">
-                                    Organised Support for Your Mental Well-Being" <br />
-                                    At New Hope Hospice Care, we recognize the value of planned visits in ensuring consistent
-                                    and dependable care. Our staff is dedicated to keeping a structured and consistent
-                                    visitation schedule, giving patients and their families peace of mind.
-                                    <br />
-                                    <br />
-                                    We are able to keep an eye on changing requirements and adjust our compassionate
-                                    care accordingly, all while honoring each person's distinctive path, thanks to
-                                    these scheduled visits. You can rely on New Hope to be there for you during the
-                                    most trying times of life, offering consolation and encouragement."
-                                </div>
-                                <div className="buttons_line">
-                                    <a href="/#" onClick={(e)=>handleClickSchedul(e)} className="site_btn">Schedul</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="goals_section  service_block inlineImg_section second_bg" id="service3" >
-                    <div className="inlineImg_container">
-                        <div className="image_block absoluite_image" style={{ backgroundImage: `url(${serviveInner3})` }}></div>
-                        <div className="goal_info page_section">
-                            <div className="info_inner">
-                                <div className="section_title service_title">
-                                    Scheduled visits
-                                </div>
-                                <div className="section_description">
-                                    Organised Support for Your Mental Well-Being" <br />
-                                    At New Hope Hospice Care, we recognize the value of planned visits in ensuring consistent
-                                    and dependable care. Our staff is dedicated to keeping a structured and consistent
-                                    visitation schedule, giving patients and their families peace of mind.
-                                    <br />
-                                    <br />
-                                    We are able to keep an eye on changing requirements and adjust our compassionate
-                                    care accordingly, all while honoring each person's distinctive path, thanks to
-                                    these scheduled visits. You can rely on New Hope to be there for you during the
-                                    most trying times of life, offering consolation and encouragement."
-                                </div>
-                                <div className="buttons_line">
-                                <a href="/#" onClick={(e)=>handleClickSchedul(e)} className="site_btn">Schedul</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="goals_section  service_block inlineImg_section second_bg" id="service4" >
-                    <div className="inlineImg_container">
-                        <div className="image_block absoluite_image" style={{ backgroundImage: `url(${serviveInner4})` }}></div>
-                        <div className="goal_info page_section">
-                            <div className="info_inner">
-                                <div className="section_title service_title">
-                                    Scheduled visits
-                                </div>
-                                <div className="section_description">
-                                    Organised Support for Your Mental Well-Being" <br />
-                                    At New Hope Hospice Care, we recognize the value of planned visits in ensuring consistent
-                                    and dependable care. Our staff is dedicated to keeping a structured and consistent
-                                    visitation schedule, giving patients and their families peace of mind.
-                                    <br />
-                                    <br />
-                                    We are able to keep an eye on changing requirements and adjust our compassionate
-                                    care accordingly, all while honoring each person's distinctive path, thanks to
-                                    these scheduled visits. You can rely on New Hope to be there for you during the
-                                    most trying times of life, offering consolation and encouragement."
-                                </div>
-                                <div className="buttons_line">
-                                <a href="/#" onClick={(e)=>handleClickSchedul(e)} className="site_btn">Schedul</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            ))}
                 <div className="button_block">
                     <button className="more_services">
                         More services {'>'}
